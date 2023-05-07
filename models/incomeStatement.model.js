fdb013519f17b4d595d47976c97eb96a3da2469c
Anjani Priyanka Neeli <anjanipriyanka01@gmail.com>
2023-05-07 19:32:43 +0530

    getbycid-done,deletebycid-done,putbycid-inprogress

--- models/incomeStatement.model.js
+++ models/incomeStatement.model.js
@@ -4,20 +4,26 @@ const Schema = mongoose.Schema;
 const incomeStatementSchema = new Schema({
   companyId: {
     type: String,
+    unique:true,
+    require:true
   },
   year: {
     type: Number,
+    require:true
   },
   revenue: [
     {
       product: {
         type: Number,
+        require:true
       },
       services: {
         type: Number,
+        require:true
       },
       totalNet: {
         type: Number,
+        require:true
       },
     },
   ],
@@ -25,71 +31,89 @@ const incomeStatementSchema = new Schema({
     {
       product: {
         type: Number,
+        require:true
       },
       services: {
         type: Number,
+        require:true
       },
       total: {
         type: Number,
+        require:true
       },
     },
   ],
   grossMargin: {
     type: Number,
+    require:true
   },
   operatingExpenses: [
     {
       researchAndDevelopment: {
         type: Number,
+        require:true
       },
       sellingGeneralAndAdministrative: {
         type: Number,
+        require:true
       },
       otherItems: [
         {
           type: Number,
+          require:true
         },
       ],
       total: {
         type: Number,
+        require:true
       },
     },
   ],
   operatingIncome: {
     type: Number,
+    require:true
   },
   nonOperatingExpenses: [
     {
       interestAndDividendIncome: {
         type: Number,
+        require:true
       },
       interestExpense: {
         type: Number,
+        require:true
       },
       otherIncomeOrExpenseNet: {
         type: Number,
+        require:true
       },
       netInterestIncomeOrExpense: {
         type: Number,
+        require:true
       },
       otherItems: [
         {
           type: Number,
+          require:true
         },
       ],
       total: {
         type: Number,
+        require:true
       },
     },
   ],
   earningsBeforeTax: {
     type: Number,
+    require:true
   },
   provisionForTax: {
     type: Number,
+    require:true
   },
   netIncome: {
     type: Number,
+    require:true
   },
 });
 
