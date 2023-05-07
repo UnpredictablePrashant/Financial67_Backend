86bc20b3d36ad74950792af127e84d7627fe8fa4
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-07 19:05:46 +0530

    controller of confidential

---
+++ controllers/confidential.controller.js
@@ -0,0 +1,69 @@
+const Confidential = require("../models/confidential.model");
+
+exports.createConfidential = async (req, res) => {
+    try {
+        const {
+            EquityDeployed,
+            Debt_Burden,
+            Cost_of_Debt,
+            Non_Interest_liabilities_Risk,
+            Total_Deployed_Capital,
+            Cash_And_Equivalents,
+            Interest_Differential,
+            Dya_Receivable_Outstanding,
+            Receivable_Turnover_Ratio,
+            Days_Payables_Outstanding,
+            Pyable_Turnover_Ratio,
+            Index_Base_Value,
+            Index_Value_Scaled_To_100,
+        } = req.body;
+        const confidentialData = new Confidential({
+            EquityDeployed,
+            Debt_Burden,
+            Cost_of_Debt,
+            Non_Interest_liabilities_Risk,
+            Total_Deployed_Capital,
+            Cash_And_Equivalents,
+            Interest_Differential,
+            Dya_Receivable_Outstanding,
+            Receivable_Turnover_Ratio,
+            Days_Payables_Outstanding,
+            Pyable_Turnover_Ratio,
+            Index_Base_Value,
+            Index_Value_Scaled_To_100,
+        });
+
+        const newConfidential = await confidentialData.save();
+        res.status(201).json(newConfidential);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: "Error creating confidentials", error: error });
+    }
+};
+
+
+exports.getAllConfidential = async (req, res) => {
+    try {
+        const confidential = await Confidential.find();
+        res.status(200).json(confidential);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching company confidentials", error });
+    }
+};
+
+exports.getConfidentialById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const confidential = await Confidential.findById(id);
+
+        if (!confidential) {
+            return res.status(404).send('Confidential not found');
+        }
+
+        res.send(confidential);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+
+}
