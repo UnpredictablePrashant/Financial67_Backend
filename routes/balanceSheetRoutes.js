f81f3af6ffb57ea2fc42fdbaa5f0b6df030baebc
Prashant Kumar Dey <prashantsavior@gmail.com>
2023-05-07 18:56:04 +0530

    FIX: updating the name of the file

--- routes/balanceSheetRoutes.js
+++ routes/balanceSheetRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createBalanceSheet, getAllBalanceSheets } = require("../controllers/balanceSheetController");
+const { createBalanceSheet, getAllBalanceSheets } = require("../controllers/balanceSheet.controller");
 
 router.post("/create", createBalanceSheet);
 router.get("/", getAllBalanceSheets);
