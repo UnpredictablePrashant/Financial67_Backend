31d7422ab639d127b1c7a52b30145fcc4a75c6a0
Karnika <113451373+Karnikabora@users.noreply.github.com>
2023-08-25 21:06:10 +0530

    Revert "first commit"
    This reverts commit eab71f3dc9d992d8096c991ab641ad26a24e7082.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const checkRoutes = require("./routes/check.routes");
 const dbconnect = require("./conn/db.conn");
 const balanceSheetRoutes = require('./routes/balanceSheetRoutes');
 const incomeStatementRoutes = require('./routes/incomeStatementRoutes');
-const profitabilityRoutes = require('./routes/profitability.routes')
+
 const cashflowStatementRoutes = require('./routes/cashflowStatement.routes');
 const companyRoutes = require('./routes/company.route')
 const confidentialRoutes = require('./routes/confidential.routes')
@@ -22,7 +22,7 @@ app.use('/balancesheet', balanceSheetRoutes);
 app.use('/incomestatement', incomeStatementRoutes);
 app.use('/confidential', confidentialRoutes);
 app.use('/cashflowstatement', cashflowStatementRoutes);
-app.use("/profitability", profitabilityRoutes);
+
 app.listen(process.env.PORT || 3001, () => {
   dbconnect();
   console.log(`listening on http://localhost:${process.env.PORT}`);
